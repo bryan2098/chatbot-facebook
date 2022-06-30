cc92cf34f093a7699b96fc2338a16d3d5a7122b3
Tri Nguyen <triminh.nguyen@isobar.com>
2022-06-30 23:51:14 +0700

    fix

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -102,7 +102,7 @@ async function handlePostback(sender_psid, received_postback) {
             break;
 
         case 'GET_STARTED':
-            await chatbotService.handleGetStarted();
+            await chatbotService.handleGetStarted(sender_psid);
             break;
 
         default:
--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -3,7 +3,7 @@ import request from "request";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-let callSendAPI = (response) => {
+let callSendAPI = (sender_psid, response) => {
     // Construct the message body
     let request_body = {
         "recipient": {
@@ -31,7 +31,7 @@ let handleGetStarted = () => {
     return new Promise(async (resolve, reject) => {
         try {
             let response = { 'text': 'Xin chào mừng bạn đến với Mollie Shop' };
-            await callSendAPI(response);
+            await callSendAPI(sender_psid, response);
             resolve('done');
         } catch (error) {
             reject(error)
