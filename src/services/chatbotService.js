4716ab7fcb5bee299e326ece69f76d18bb006b4f
Tri Nguyen <triminh.nguyen@isobar.com>
2022-07-02 13:50:50 +0700

    add username

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -27,10 +27,32 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
+let getUserName = async (sender_psid) => {
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "GET",
+    }, (err, res, body) => {
+        console.log('body', body);
+
+        if (!err) {
+            let response = JSON.parse(res);
+            username = `${response.first_name} ${response.last_name}`;
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+
+    return username;
+}
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let response = { 'text': 'Xin chào mừng bạn đến với Mollie Shop' };
+            let username = await getUserName(sender_psid);
+            let response = { 'text': `Xin chào mừng ${username} đến với Mollie Shop` };
             await callSendAPI(sender_psid, response);
             resolve('done');
         } catch (error) {
