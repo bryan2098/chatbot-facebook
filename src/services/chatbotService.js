a599a08a08baa5f2db5189bded7182e99c80d786
Tri Nguyen <triminh.nguyen@isobar.com>
2022-07-18 15:37:19 +0700

    fix

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const common = require('../script/common');
 const CFGBTN = require('../configs/btn.json');
+const CFGBTNJS = require('../configs/btnConfig');
 
 const IMAGES = [
     'https://res.cloudinary.com/dvweth7yl/image/upload/v1656778684/product/z3533592465888_34e9848417e25ad68aea56a81c56d115.jpg',
@@ -40,13 +41,7 @@ let getListProductTemplate = (sender_psid) => {
             "subtitle": "Tất cả các ngày trong tuần",
             "image_url": IMAGES[0],
             "buttons": [
-                {
-                    "type": "web_url",
-                    "url": `${process.env.URL_WEB_VIEW_ORDER}?senderId=${sender_psid}`,
-                    "title": "Mua sản phẩm",
-                    "webview_height_ratio": "tall",
-                    "messenger_extensions": true
-                },
+                CFGBTNJS.btnBuyProduct(sender_psid),
             ],
         },
         {
@@ -74,14 +69,7 @@ let getSetListTemplate = () => {
                 CFGBTN.PRODUCT.DETAIL,
             ],
         },
-        {
-            "title": "Quay trở lại",
-            "subtitle": "Quay trở lại danh sách sản phẩm",
-            "image_url": IMAGES[0],
-            "buttons": [
-                CFGBTN.COMMON.BACK_TO_LIST
-            ],
-        }
+        CFGBTNJS.btnBackToList()
     ];
 
 
@@ -109,14 +97,7 @@ let getProductDetailTemplate = () => {
                 CFGBTN.PRODUCT.SHOW_IMAGE,
             ],
         },
-        {
-            "title": "Quay trở lại",
-            "subtitle": "Quay trở lại danh sách sản phẩm",
-            "image_url": IMAGES[0],
-            "buttons": [
-                CFGBTN.COMMON.BACK_TO_LIST
-            ],
-        }
+        CFGBTNJS.btnBackToList()
     ];
 
     return common.getTemplate(elements, "generic");
@@ -133,6 +114,39 @@ let getStartedQuickReplyTemplate = () => {
 
         ]
     };
+
+    return response;
+}
+
+let getImageTemplate = () => {
+    let response = {
+        "attachment": {
+            "type": "image",
+            "payload": {
+                "url": IMAGES[0],
+                "is_reusable": true
+            }
+        }
+    };
+
+    return response;
+}
+
+
+let getButtonTemplate = (sender_psid) => {
+    let response = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text": "Size shop < 58kg nha bạn iu",
+                "buttons": [
+                    CFGBTN.STARTED.PRODUCT_LIST,
+                    CFGBTNJS.btnBuyProduct(sender_psid),
+                ]
+            }
+        }
+    }
 
     return response;
 }
@@ -320,50 +334,6 @@ let handleDetailProduct = (sender_psid) => {
     })
 }
 
-
-let getImageTemplate = () => {
-    let response = {
-        "attachment": {
-            "type": "image",
-            "payload": {
-                "url": IMAGES[0],
-                "is_reusable": true
-            }
-        }
-    };
-
-    return response;
-}
-
-
-let getButtonTemplate = (sender_psid) => {
-    let response = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": "Size shop < 58kg nha bạn iu",
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "DANH SÁCH SẢN PHẨM NỔI BẬT",
-                        "payload": "PRODUCT_LIST",
-                    },
-                    {
-                        "type": "weburl",
-                        "url": `${process.env.URL_WEB_VIEW_ORDER}?senderId=${sender_psid}`,
-                        "title": "Mua sản phẩm",
-                        "webview_height_ratio": "tall",
-                        "messenger_extensions": true
-                    },
-                ]
-            }
-        }
-    }
-
-    return response;
-}
-
 let handleShowImage = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -381,54 +351,6 @@ let handleShowImage = (sender_psid) => {
         }
     })
 }
-
-
-// let handleGuideToUseBot = (sender_psid) => {
-//     return new Promise(async (resolve, reject) => {
-//         try {
-
-//             let username = await common.getUserName(sender_psid);
-//             let responseText = {
-//                 text: `Xin chào bạn ${username}, mình là chat bot Mollie. \n Để biết cách sử dụng, bạn vui lòng xem hết video bên dưới nhé 😉`
-//             };
-
-//             let responseMediaTemplate = getBotMediaTemplate();
-
-//             await common.callSendAPI(sender_psid, responseText);
-//             await common.callSendAPI(sender_psid, responseMediaTemplate);
-
-//             resolve('done');
-//         } catch (error) {
-//             reject(error)
-//         }
-//     })
-// }
-
-// let getBotMediaTemplate = () => {
-//     let elements = [
-//         {
-//             "media_type": "<image|video>",
-//             // "attachment_id": "765420714370507",
-//             // Need upload video to https://business.facebook.com/creatorstudio/content_posts after that copy link video
-//             "url": "https://business.facebook.com/hoidanITEricRestaurant/videos/765420714370507",
-//             "buttons": [
-//                 {
-//                     "type": "postback",
-//                     "title": "Danh sách sản phẩm nổi bật",
-//                     "payload": "PRODUCT_LIST",
-//                 },
-//                 {
-//                     "type": "weburl",
-//                     "url": `https://shopee.vn/mollieshop2501`,
-//                     "title": "Shopee",
-//                     "webview_height_ratio": "full",
-//                 },
-//             ]
-//         }
-//     ];
-
-//     return common.getTemplate(elements, 'media');
-// }
 
 module.exports = {
     handleGetStarted: handleGetStarted,
