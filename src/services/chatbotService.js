3205226b1acdd1ec19a7594c6feb9d6db45aac2a
Tri Nguyen <triminh.nguyen@isobar.com>
2022-07-19 17:24:28 +0700

    fix

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -395,25 +395,11 @@ let handleSendPolicy = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
 
-            let templatePolicyBuyProduct = {
-                text: ` -- CHÍNH SÁCH MUA HÀNG --
-                \n - Miễn ship từ 2 sản phẩm trở lên
-                \n - Giảm 5-10% khi mua từ 3 sản phẩm trở lên
-                \n - Giảm 15-20% khi mua từ 5 sản phẩm trở lên và trở thành khách hàng thân thiết của Mollie 
-                `
-            };
+            // chinh sach mua hang
+            await common.callSendAPI(sender_psid, template.templateBuyProductPolicy());
 
-            await common.callSendAPI(sender_psid, templatePolicyBuyProduct);
-
-            let templateReturnPolicy = {
-                text: ` -- CHÍNH SÁCH ĐỔI TRẢ --
-                \n - Trả hàng khi shop giao nhầm size hoặc không đúng mẫu (shop trả phí)
-                \n - Đổi trả sản phẩm khi sản phẩm khi bị lỗi (shop trả phí)
-                \n (Chỉ xử lý 2 trường hợp trên khi có quay video khui hàng để tránh trường hợp không phải lỗi của shop - trả hàng từ 2-3 ngày sau khi nhận hàng)
-                `
-            };
-
-            await common.callSendAPI(sender_psid, templateReturnPolicy);
+            // chinh sach doi tra
+            await common.callSendAPI(sender_psid, template.templateReturnPolicy());
 
             resolve('done');
         } catch (error) {
@@ -426,27 +412,7 @@ let handleSendPaymentMethod = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
 
-            let templatePaymentMethod = {
-                text: ` -- PHƯƠNG THỨC THANH TOÁN --
-                \n Chuyển khoản ngân hàng:
-                \n    *Ngân hàng: Sacombank
-                \n    *STK: 060266421225
-                \n    *Tên: NGUYEN THI NHAT TRINH
-                \n
-                \n    *Ngân hàng: Techcombank
-                \n    *STK: 19036205125014
-                \n    *Tên: NGUYEN MINH TRI
-                \n -Nội dung chuyểnh khoản-
-                \n Họ tên - Số điện thoại
-                \n (Vui lòng chụp màn hình lại và gửi shop để xác nhận thanh toán)
-                \n
-                \n COD: Ship toàn quốc
-                \n   *Miễn ship nội thành (TP.HCM)
-                \n   *Ngoại thành phí ship là 30.000đ
-                `
-            };
-
-            await common.callSendAPI(sender_psid, templatePaymentMethod);
+            await common.callSendAPI(sender_psid, template.templatePaymentMethod());
 
             resolve('done');
         } catch (error) {
--- src/services/templateService.js
+++ src/services/templateService.js
@@ -19,7 +19,52 @@ const templateInfoProduct = () => {
     };
 }
 
+const templatePaymentMethod = () => {
+    return {
+        text: ` -- PHƯƠNG THỨC THANH TOÁN --
+        \n Chuyển khoản ngân hàng:
+        \n    *Ngân hàng: Sacombank
+        \n    *STK: 060266421225
+        \n    *Tên: NGUYEN THI NHAT TRINH
+        \n
+        \n    *Ngân hàng: Techcombank
+        \n    *STK: 19036205125014
+        \n    *Tên: NGUYEN MINH TRI
+        \n -Nội dung chuyểnh khoản-
+        \n Họ tên - Số điện thoại
+        \n (Vui lòng chụp màn hình lại và gửi shop để xác nhận thanh toán)
+        \n
+        \n COD: Ship toàn quốc
+        \n   *Miễn ship nội thành (TP.HCM)
+        \n   *Ngoại thành phí ship là 30.000đ
+        `
+    }
+}
+
+const templateBuyProductPolicy = () => {
+    return {
+        text: ` -- CHÍNH SÁCH MUA HÀNG --
+        \n - Miễn ship từ 2 sản phẩm trở lên
+        \n - Giảm 5-10% khi mua từ 3 sản phẩm trở lên
+        \n - Giảm 15-20% khi mua từ 5 sản phẩm trở lên và trở thành khách hàng thân thiết của Mollie 
+        `
+    };
+}
+
+const templateReturnPolicy = () => {
+    return {
+        text: ` -- CHÍNH SÁCH ĐỔI TRẢ --
+        \n - Trả hàng khi shop giao nhầm size hoặc không đúng mẫu (shop trả phí)
+        \n - Đổi trả sản phẩm khi sản phẩm khi bị lỗi (shop trả phí)
+        \n (Chỉ xử lý 2 trường hợp trên khi có quay video khui hàng để tránh trường hợp không phải lỗi của shop - trả hàng từ 2-3 ngày sau khi nhận hàng)
+        `
+    }
+};
+
 module.exports = {
     templateInfoShop: templateInfoShop,
-    templateInfoProduct: templateInfoProduct
+    templateInfoProduct: templateInfoProduct,
+    templatePaymentMethod: templatePaymentMethod,
+    templateBuyProductPolicy: templateBuyProductPolicy,
+    templateReturnPolicy: templateReturnPolicy
 }
