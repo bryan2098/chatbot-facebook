150500096a2d6b1b952665bb1609218582cd6c9f
Tri Nguyen <triminh.nguyen@isobar.com>
2022-07-22 16:03:01 +0700

    fix

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('postgres://wwgprdlmaisekl:7dc59340899f3c2a7b393851821e81dd5a57b6ada1ec8018a888b2805279023a@ec2-3-217-14-181.compute-1.amazonaws.com:5432/d3ffoorkgietmi') // Example for postgres
+// const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/ChatBotMollie') // Example for postgres
 
 const connect = async () => {
     try {
