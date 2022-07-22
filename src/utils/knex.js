7a82b7e41399f7bb9e8ed0711792f44ab0ed73ad
Tri Nguyen <triminh.nguyen@isobar.com>
2022-07-22 15:14:00 +0700

    f

--- src/utils/knex.js
+++ src/utils/knex.js
@@ -20,7 +20,7 @@ const knex = require('knex')({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        ssl: false
+        ssl: true
     },
     pool: {
         min: 1,
