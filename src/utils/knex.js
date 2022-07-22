8996b1de8c66f33dce80f842f92ac0b488a72ef8
Tri Nguyen <triminh.nguyen@isobar.com>
2022-07-22 15:06:05 +0700

    f

--- src/utils/knex.js
+++ src/utils/knex.js
@@ -24,11 +24,6 @@ const knex = require('knex')({
     pool: {
         min: 1,
         max: 100,
-        createTimeoutMillis: 3000,
-        acquireTimeoutMillis: 30000,
-        idleTimeoutMillis: 30000,
-        reapIntervalMillis: 1000,
-        createRetryIntervalMillis: 100,
         propagateCreateError: false
     },
     useNullAsDefault: true
